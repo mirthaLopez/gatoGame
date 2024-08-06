95877e450773794f434417a3872afb32b3ec9134
mirthaLopez <melopez@fwdcostarica.com>
2024-08-06 15:59:34 -0600

    TurnoComputadora

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,24 +9,23 @@ for (let index = 0; index < casillas.length; index++) {
     casillas[index].addEventListener("click", function () {
         if (casillas[index].textContent == "") {
             casillas[index].innerHTML = "X";
-            listaDisponibles = listaPosiciones.filter(espacios => espacios != index)
+            listaDisponibles = listaPosiciones.filter(espacios => espacios != index);
             console.log(listaDisponibles);
-            listaPosiciones = listaPosiciones.filter(espacios => espacios != index)
+            listaPosiciones = listaPosiciones.filter(espacios => espacios != index);
             if (listaDisponibles.length>1) {
                 let valorRam = Math.floor((Math.random() * listaDisponibles.length));
                 console.log(valorRam);
                 let posicion=listaDisponibles[valorRam];
                 console.log(posicion);
                 casillas[posicion].innerHTML = "O";
-                listaDisponibles = listaPosiciones.filter(espacios => espacios != posicion)
+                listaDisponibles = listaPosiciones.filter(espacios => espacios != posicion);
                 console.log(listaDisponibles);
-                listaPosiciones = listaPosiciones.filter(espacios => espacios != posicion);   
+                listaPosiciones = listaPosiciones.filter(espacios => espacios != posicion);
+                
             }
             
         } else {
             textoAdvertencia.innerHTML = "Casilla llena, elige otra casilla";
         }
-
     })
-
 }
